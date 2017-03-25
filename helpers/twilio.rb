cfdbfe394f104dcebbe29444ca40a537224af28a
Jared Murphy <jaredcmurphy@gmail.com>
2017-03-25 00:55:53 -0400

    update twilio env vars

--- helpers/twilio.rb
+++ helpers/twilio.rb
@@ -1,7 +1,7 @@
 class TwilioHelper
  def initialize
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token  = ENV['TWILIO_AUTH_TOKEN']
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token  = ENV["TWILIO_AUTH_TOKEN"]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
@@ -30,8 +30,8 @@ class TwilioHelper
 
   def sms(message)
     @client.messages.create(
-      from:      ENV['TWILIO_NUMBER'],
-      to:        ENV['CLIENT_NUMBER'],
+      from:      ENV["TWILIO_NUMBER"],
+      to:        ENV["CLIENT_NUMBER"],
       body:      message
     )
   end
